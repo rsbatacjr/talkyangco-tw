1a0c64845414e9fd82e3488d0d795663a7b7f4ed
rey <reybatacjr@gmail.com>
2017-05-20 13:03:17 +0800

    Initial commit

---
+++ footer.php
@@ -0,0 +1,135 @@
+			<div class="row">
+				<footer>
+					<div id="footer-menu">
+						<div class="container">
+							<div class="row">
+								<div class="col-md-2">
+									<ul>
+										<li><a href="">介绍</a></li>
+										<li>&nbsp;</li>
+										<li><a href="">关于我们</a></li>
+										<li><a href="">问候</a></li>
+										<li><a href="">优势</a></li>
+										<li><a href="">位置</a></li>
+									</ul>
+								</div>
+								<div class="col-md-2">
+									<ul>
+										<li><a href="">過程</a></li>
+										<li>&nbsp;</li>
+										<li><a href="">程序</a></li>
+										<li><a href="">关于准备</a></li>
+										<li><a href="">FAQ</a></li>
+										<li><a href="">医药和保险系统</a></li>
+										<li><a href="">如何来碧瑶</a></li>
+										<li><a href="">竞技</a></li>
+									</ul>
+								</div>
+								<div class="col-md-2">
+									<ul>
+										<li><a href="">注册</a></li>
+										<li>&nbsp;</li>
+										<li><a href="">学费</a></li>
+										<li><a href="">宿舍类型</a></li>
+										<li><a href="">其他费用</a></li>
+										<li><a href="">IELTS官方考试日期</a></li>
+										<li><a href="">TOEIC官方考试日期</a></li>
+										<li><a href="">菲律宾假日</a></li>
+										<li><a href="">官方接机日</a></li>
+										<li><a href="">退费政策</a></li>
+										<li><a href="">电子申请书(现在注册)</a></li>
+									</ul>
+								</div>
+								<div class="col-md-2">
+									<ul>
+										<li><a href="">Talk的课程</a></li>
+										<li>&nbsp;</li>
+										<li><a href="">ESL 421, 521, 611</a></li>
+										<li><a href="">强化 IELTS</a></li>
+										<li><a href="">强化 TOEIC</a></li>
+										<li><a href="">IELTS 保证班</a></li>
+										<li><a href="">TOEIC 保证班</a></li>
+										<li><a href="">强化口说</a></li>
+										<li><a href="">打工度假</a></li>
+										<li><a href="">家庭 ESL</a></li>
+										<li><a href="">联系我们</a></li>
+									</ul>
+								</div>
+								<div class="col-md-2">
+									<ul>
+										<li><a href="">联系我们</a></li>
+										<li>&nbsp;</li>
+										<li><a href="">TOEIC 考试</a></li>
+										<li><a href="">IELTS 考试</a></li>
+										<li><a href="">强化口说</a></li>
+										<li><a href="">短期(1-3周)学习</a></li>
+										<li><a href="">长期学习</a></li>
+										<li><a href="">最优价格</a></li>
+										<li><a href="">组合在菲律宾，澳大利亚和加拿大学习英语</a></li>
+									</ul>
+								</div>
+								<div class="col-md-2">
+									<ul>
+										<li><a href="">社区</a></li>
+										<li>&nbsp;</li>
+										<li><a href="">公告</a></li>
+										<li><a href="">学生经理</a></li>
+										<li><a href="">Talk’每餐</a></li>
+										<li><a href="">在菲律賓</a></li>
+										<!-- <li><a href="">Best Student &amp; Teacher</a></li> -->
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row" style="margin-top: 30px; margin-bottom: 30px;">
+							<div class="footer-icons col-xs-6 col-sm-3 text-center">
+								<img src="<?php echo THEME_URI ?>/img/bureau-immigration.png" style="width: 90px; height: 90px;">
+								<p>SSP<br>(特別學習許可證)</p>
+							</div>
+							<div class="footer-icons col-xs-6 col-sm-3 text-center">
+								<img src="<?php echo THEME_URI ?>/img/tesda.png" style="width: 90px; height: 90px;">
+								<p>TESDA</p>
+							</div>
+							<div class="footer-icons col-xs-6 col-sm-3 text-center">
+								<img src="<?php echo THEME_URI ?>/img/pcc.png" style="width: 90px; height: 90px;">
+								<p>TVET認證證書 </p>
+							</div>
+							<div class="footer-icons col-xs-6 col-sm-3 text-center">
+								<img src="<?php echo THEME_URI ?>/img/besa.png" style="width: 90px; height: 90px;">
+								<p>碧瑤英語學校協會</p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-6">
+								<p>
+									Yangco Center<br>
+									Tel : +63-74-442-6012<br>
+									Address : #37 yangco Rd., Upper General Luna Rd., Baguio City, 2600 Philippines<br>
+									President Kenneth : +63-917-568-9161
+								</p>
+							</div>
+							<div class="col-md-6">
+								<p>
+									EnE Center<br>
+									Tel : +63-74-442-6012<br>
+									Address : Lot A, Dacanay Building, Marcos Highway, Baguio City, 2600 Philippines<br>
+									President Louis : +63-917-688-8656
+								</p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12 text-center">
+								<p><br/><br/>
+								<a href="mailto:baguiotalk@gmail.com">baguiotalk@gmail.com</a> &copy; <i>Canitalk Academy Inc. All rights reserved.</i></p>
+							</div>
+						</div>
+					</div> 
+				</footer>
+			</div>
+		</div>
+	</body>
+	<script type="text/javascript" src="<?php echo THEME_URI ?>/js/jquery.min.js"></script>
+	<script type="text/javascript" src="<?php echo THEME_URI ?>/js/bootstrap.min.js"></script>
+</html>
---
+++ home.php
@@ -0,0 +1,268 @@
+<?php
+get_header();
+?>
+<div class="container">
+	<div class="row">
+		<div id="qualified-programs-container" class="col-xs-12 col-md-8">
+			<div id="qualified-programs">
+				<div class="mask">
+					<h1>英語學習者的優質項目</h1>
+				<p style="max-width: 400px;">TALK學院為希望提高英語水平，準備考試，申請工作和移民到其他國家的學生提供各種英語課程。</p>
+				<div id="course-builder-container">
+					<div class="row">
+						<div class="col-xs-12">
+							<strong>&nbsp;課程分類  :</strong>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-12 col-sm-6 col-md-3">
+							<div class="course-builder">
+								<strong>口語課程</strong>
+								<p>ESL 521</p>
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-6 col-md-3">
+							<div class="course-builder">
+								<strong>職業申請</strong>
+								<p>強化 TOEIC</p>
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-6 col-md-3">
+							<div class="course-builder">
+								<strong>大學&amp;移民</strong>
+								<p>強化 IELTS</p>
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-6 col-md-3">
+							<div class="course-builder">
+								<strong>家庭課程</strong>
+								<p>家庭 ESL</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+		<div id="about-container" class="col-xs-12 col-sm-6 col-md-4">
+			<div id="about">
+				<div class="mask">
+					<h2>關於TALK</h2>
+					<div id="about-normal">
+						<p>預覽TALK的教室和宿舍</p>
+						<span class="plus-sign">+</span>
+						<div class="clearfix"></div>
+					</div>
+					<div id="about-hover">
+						<p>
+						在TALK学院开始学习英语之前，您可以在3D中查看TALK的教室，宿舍和其他设施。如果您提前了解重要信息，您将有益于您在菲律宾留学
+						</p>
+						<span style="position: absolute;bottom: 10px;right: 10px;display: inline-block;margin-bottom: 0;" class="next-button"></span>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+		<div id="experience-container" class="col-xs-12 col-sm-6 col-md-4">
+			<div id="experience">
+				<div class="mask">
+					<h2>學生經歷</h2>
+					<div id="experience-normal">
+						<p>在 TALK 學院分享你的生活</p>
+						<span class="plus-sign">+</span>
+					</div>
+					<div id="experience-hover">
+						<p>
+							在您决定在TALK学院学习英语之前，我们建议您阅读TALK学生的文章，其中包含他们在菲律宾学习，留学和旅行的文章。 他们想与新生分享他们的经验！
+						</p>
+						<span style="position: absolute;bottom: 10px;right: 10px;display: inline-block;margin-bottom: 0;" class="next-button"></span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div id="notice-container" class="col-xs-12 col-md-4">
+			<div id="notice">
+				<strong>公告</strong>
+				<p>
+					在学习Talk之前，您可以查看TALK的消息。 这将有助于您决定在TALK学院学习英语
+				</p>
+				<div id="notice-list-container">
+					<div class="row">
+						<div class="col-xs-12">
+							<span id="notice-plus">+</span>
+							<span class="next-button"></span>
+						</div>
+					</div>
+					<ul id="notice-list">
+						<li><a href="#">5월 입학가능일 안내</a></li>
+						<li><a href="#">노용이 화난 날(Warning)</a></li>
+						<li><a href="#">노용이 소리 질른 날..ㅋㅋ</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div id="plans-container" class="col-xs-12 col-md-8">
+			<div id="plans">
+				<div class="mask">
+					<strong>計劃在菲律賓學習英語</strong>
+					<div id="plan-list-container" class="row">
+						<div class="row">
+							<div class="col-xs-12">
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>IELTS<br>5.0, 5.5, 6.0</p>
+											<span class="plus-sign">+</span>
+										</div>	
+										<div class="plan-list-hover">
+											<p>让我们一起为进入大学做准备或者移民。TALK帮助你达到理想的雅思成绩. </p>
+											<span class="next-button-black"></span>
+										</div>
+									</div>
+								</div>
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>強化口說</p>
+											<span class="plus-sign">+</span>
+										</div>	
+										<div class="plan-list-hover">
+											<p>即使你擅长语法和词汇，但是你的发音还不完美，我们建议你上我们的强化口说课程.</p>
+											<span class="next-button-black"></span>
+										</div>
+									</div>
+								</div>
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>短期(1-3週)<br>學習</p>
+											<span class="plus-sign">+</span>
+										</div>	
+										<div class="plan-list-hover">
+											<p>如果您没有足够的时间学习英语或不了解英语学习方式，我们会引导您一步步学习英语..</p>
+											<span class="next-button-black"></span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-xs-12">
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>TOEIC<br>600, 700, 800, 900</p>
+											<span class="plus-sign">+</span>
+										</div>
+										<div class="plan-list-hover">
+											<p>让我们借助TALK学院高效的获得TOEIC成绩。 第一所在菲律宾开办TOEIC成绩保证班的学校</p>
+											<span class="next-button-black"></span>
+										</div>
+									</div>
+								</div>
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p>组合在菲律宾，澳大利亚和加拿大学习英语</p>
+											<span class="plus-sign">+</span>
+										</div>
+										<div class="plan-list-hover">
+											<p>如果你感觉到，在你开始上大学之前，你需要学习英语，为什么不和我们一起学习英语？</p>
+											<span class="next-button-black"></span>
+										</div>
+										
+									</div>
+								</div>
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>TALK學院輕鬆的英語學習</p>
+											<span class="plus-sign">+</span>
+										</div>
+										<div class="plan-list-hover">
+											<p>你觉得学费是贵吗？<br>我们为学习者提供了英语课程的预算。<br>只需联系我们</p>
+											<span class="next-button-black"></span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div id="best-place-container" class="col-xs-12 col-md-4">
+			<div id="best-place">
+				<div class="mask">
+					<strong>旅游的最佳地点</strong>
+					<div id="best-place-list-container">
+						<ul id="best-place-list">
+							<li><a href="#">碧瑶</a></li>
+							<li><a href="#">一日游游览路线</a></li>
+							<li><a href="#">1-2日游游览路线</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="campaign-container" class="col-xs-12 col-md-4">
+			<div id="campaign">
+				<strong>竞技</strong>
+				<div id="campaign-list-container">
+					<ul id="campaign-list">
+						<li><a href="#">与欧美外教一起准备IELTS</a></li>
+						<li><a href="#">12周内TOEIC提高300分</a></li>
+						<li><a href="#">TALK学校官方TOEIC考试免费</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div id="calendar-container" class="col-xs-12 col-md-4">
+			<div id="calendar">
+				<strong>日历</strong>
+				<div id="calendar-list-container">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="calendar-list-parent col-xs-6" style="border-right:1px solid #666; border-bottom: 1px solid #666">
+								<div class="calendar-list">
+									<p><br>菲律宾假日</p>
+								</div>
+							</div>
+							<div class="calendar-list-parent col-xs-6" style=" border-bottom: 1px solid #666">
+								<div class="calendar-list">
+									<p><br>官方接机日</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="calendar-list-parent col-xs-6" style="border-right:1px solid #666">
+								<div class="calendar-list">
+									<p><br>IELTS <br>官方考试日期</p>
+								</div>
+							</div>
+							<div class="calendar-list-parent col-xs-6">
+								<div class="calendar-list">
+									<p><br>TOEIC<br>官方考试日期</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<img src="<?php echo THEME_URI; ?>/img/sample-image1.png" class="img-responsive"/>
+</div>
+<?php 
+get_footer();
